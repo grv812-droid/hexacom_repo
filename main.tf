a1f116a3eae5d53d4356da927fad8a26d52c6bdc
grv812-droid <grv812@gmail.com>
2026-07-18 01:58:49 +0530

    rg22

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
         version = "4.80.0"
     }
   }
-}
+}   
 
 provider "azurerm" {
   features {
@@ -17,5 +17,10 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg1" {
   name="rg-hexaom-dev"
+  location = "eastus"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name="rg-hexaom-qa"
   location = "eastus"
 }
